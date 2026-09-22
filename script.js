65715465f3daed5519be790575df14a16af22c56
Favio-coder <faviusa@gmail.com>
2026-09-22 12:33:36 -0500

    Redirección de html y pantalla completa

--- script.js
+++ script.js
@@ -7,16 +7,26 @@ const btnParte2 = document.getElementById("btnParte2")
 const btnFullscreen = document.getElementById("btnFullscreen")
 
 function irParte1() {
-    alert("Funciona parte 1")
-    //window.location.href = ""
+    // alert("Funciona parte 1")
+    window.location.href = "/parte1/ruleta.html"
 }
 
 function irParte2() {
-    alert("Funciona parte 2")
-    //window.location.href = ""
+    //alert("Funciona parte 2")
+    window.location.href = "/parte2/sorteo.html"
 }
 
 /* Asignación de funciones a botones */
 btnParte1.addEventListener("click", irParte1);
 btnParte2.addEventListener("click", irParte2);
 
+// Pantalla Completa
+btnFullscreen.addEventListener("click", () => {
+
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+
+})
